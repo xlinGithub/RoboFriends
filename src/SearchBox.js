9606c95cfa7171ba90eb26944fb0ebb77a0dfa5a
xlinGithub <xiaochen.lin.2005@gmail.com>
2020-05-18 20:52:59 -0400

    front end

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = ({seasrchfield, searchChange}) => {
+    return (
+        <div className='pa2'>
+            <input type='search' 
+                placeholder='search robots' 
+                className='pa3 ba b--green bg-lightest-blue'
+                onChange={searchChange}
+            />
+        </div>
+    )
+};
+
+export default SearchBox;
