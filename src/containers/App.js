0a90092ef744f6b21510d1b3c945145d3b9febba
xlinGithub <xiaochen.lin.2005@gmail.com>
2020-05-18 21:18:15 -0400

    updated front end

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import { robots } from '../robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({
+            searchfield: event.target.value,
+        })
+    }
+
+    render() {
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        });
+        if (robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }
+    }
+};
+
+export default App;
